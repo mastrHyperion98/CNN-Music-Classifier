5afbfedb5d0df5f6142131f0cc57f08ea36f38d5
steven smith <s_smith1998@outlook.com>
2020-11-25 21:30:14 -0500

    Upload supplemental sample files and DataExtraction.py

---
+++ DataExtraction.py
@@ -0,0 +1,9 @@
+# DataExtraction.py is the script used to read all our music files and extract the required information into a csv file
+# The csv file will be fed into the CNN and it will be used to speed up testing with the neural network.
+# Reading all the music files and extracting the data is expected to take a long time.
+
+def main():
+    print("TEST")
+
+if __name__ == '__main__':
+    main()
