f374c591e53d94e49bd6aede2ba5b00630539ea2
steven smith <s_smith1998@outlook.com>
2020-11-29 16:54:59 -0500

    Create a function to extract our labels and features

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import librosa
+from sklearn.preprocessing import LabelEncoder
+
 from tools import FeatureExtractor
 import numpy as np
 import pandas as pd
 
+
+def LoadData(filename):
+    # Read our extracted data into a pandas dataframe
+    dataframe = pd.read_csv(filename)
+    # Next up we want to use sklearn preprocessing Label Encoder to turn our labels into numerics
+    genres = dataframe.iloc[:, -1]
+    # we will create an encoder
+    labelEncoder = LabelEncoder()
+    # create our y dataset
+    y = labelEncoder.fit_transform(genres)
+    # Extract and preprocess our features from the dataframe
+    X = FeatureExtractor.PreprocessData(dataframe)
+
+    return X, y
+
+
 def main():
-    print('Hello World')
+   X, y = LoadData('data.csv')
+
 
 if __name__ == '__main__':
     main()
